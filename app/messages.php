5a2441aee741534575bc40ceaaca1175fff9e0ad
Luigi D. Teixeira <luigidt@gmail.com>
2016-12-08 21:07:12 -0200

    Adicionando verbo post
    Adicionando o verbo para adicionar mensagens

--- app/messages.php
+++ app/messages.php
@@ -1,5 +1,10 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use MicroMessage\Entities\Message;
+
 $messages = $app['controllers_factory'];
 
 $messages->get('/', function () use ($app) {
@@ -9,7 +14,21 @@ $messages->get('/', function () use ($app) {
         ->select(['m.id', 'm.author', 'm.message'])
         ->getQuery()
         ->getResult(\Doctrine\ORM\AbstractQuery::HYDRATE_ARRAY);
+        
     return $app->json($messages);
+});
+
+$messages->post('/', function (Request $request) use ($app) {
+    $message = new Message();
+    $message->setAuthor($request->get('author'));
+    $message->setMessage($request->get('message'));
+
+    $app['orm.em']->persist($message);
+    $app['orm.em']->flush();
+
+    return new JsonResponse(['id' => $message->getId()], 201, [
+        'Location' => "/messages/{$message->getId()}"
+    ]);
 });
 
 return $messages;
--- tests/MicroMessage/Tests/MessagesTest.php
+++ tests/MicroMessage/Tests/MessagesTest.php
@@ -17,10 +17,10 @@ class MessagesTest extends ApplicationTest
         $this->client->request('GET', '/messages/');
         $this->assertTrue($this->client->getResponse()->isOk());
 
-        $response = json_decode($this->client->getResponse()->getContent());
-        $this->assertCount(1, $response);
+        $content = json_decode($this->client->getResponse()->getContent());
+        $this->assertCount(1, $content);
+        $message = $content[0];
 
-        $message = $response[0];
         $this->assertEquals($this->message->getId(), $message->id);
         $this->assertEquals($this->message->getAuthor(), $message->author);
         $this->assertEquals($this->message->getMessage(), $message->message);
@@ -37,5 +37,35 @@ class MessagesTest extends ApplicationTest
 
         $response = json_decode($this->client->getResponse()->getContent());
         $this->assertCount(0, $response);
+    }
+
+    public function testCreateNewMessage()
+    {
+        $this->client->request('POST', '/messages/', [
+            'author' => 'Mary Doe',
+            'message' => 'Hey John! Where are you?'
+        ]);
+        $this->assertEquals(201, $this->client->getResponse()->getStatusCode());
+
+        // Check database count changed
+        $count = $this->app['orm.em']->getRepository('MicroMessage\Entities\Message')
+            ->createQueryBuilder('m')
+            ->select('count(m.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+        $this->assertEquals(2, $count, "The entity count was not changed");
+
+        // Get new entity
+        $content = json_decode($this->client->getResponse()->getContent());
+        $message = $this->app['orm.em']->getRepository('MicroMessage\Entities\Message')
+            ->find($content->id);
+
+        // Check Location header
+        $location = $this->client->getResponse()->headers->get('Location');
+        $this->assertEquals("/messages/{$message->getId()}", $location);
+
+        // Check properties are correct
+        $this->assertEquals('Mary Doe', $message->getAuthor());
+        $this->assertEquals('Hey John! Where are you?', $message->getMessage());
     }
 }
