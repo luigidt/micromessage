5d8ea9f1220a163056007689bb17cadbccc758fd
Luigi D. Teixeira <luigidt@gmail.com>
2016-12-10 20:56:51 -0200

    Adicionando documentação no código
    Adicionando documentação mais detalhada no próprio código

--- app/messages.php
+++ app/messages.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * Neste arquivo estão todas as rotas relacionadas ao recurso mensagem,
+ * case sejam adicionados novos recursos, eles deveriam ir em um novo arquivo
+ */
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -9,6 +14,23 @@ use MicroMessage\Helpers\ViolationsHelper;
 
 $messages = $app['controllers_factory'];
 
+/**
+ * GET /
+ * Retorna uma lista com todas as mensagens publicadas
+ * Exemplo de resposta:
+ *
+ * [
+ *     {
+ *         "author": "John Doe",
+ *         "message": "Hello people!",
+ *     },
+ *     {
+ *         "author": "John Doe",
+ *         "message": "Hello people!",
+ *     },
+ * ]
+ *
+ */
 $messages->get('/', function () use ($app) {
     $messages = $app['orm.em']
         ->createQuery('SELECT m.id, m.author, m.message FROM MicroMessage\Entities\Message m')
@@ -17,6 +39,13 @@ $messages->get('/', function () use ($app) {
     return $app->json($messages);
 });
 
+/**
+ * POST /
+ * Adiciona uma nova mensagem
+ * PARÂMETROS:
+ *  - author string(32) obrigatório
+ *  - message string(140) obrigatório
+ */
 $messages->post('/', function (Request $request) use ($app) {
     $message = new Message();
     $message->setAuthor($request->get('author', null));
@@ -36,6 +65,21 @@ $messages->post('/', function (Request $request) use ($app) {
     ]);
 });
 
+/**
+ * GET /{id}
+ * Busca uma mensagem específica
+ * PARÂMETROS:
+ *  - id int obrigatório
+ *
+ * Exemplo de resposta:
+ *
+ * {
+ *     "author": "John Doe",
+ *     "message": "Hello people!",
+ * }
+ *
+ * Retorna o status 404 se a mensagem não for encontrada
+ */
 $messages->get('/{id}', function ($id) use ($app) {
     try {
         // Usando uma query para especificar os campos que devem ser retornados no JSON
@@ -50,6 +94,15 @@ $messages->get('/{id}', function ($id) use ($app) {
     }
 });
 
+/**
+ * DELETE /{id}
+ * Remove uma mensagem específica
+ * PARÂMETROS:
+ *  - id int obrigatório
+ *
+ * Retorna o status 204 se a mensagem foi removida com sucesso
+ * Retorna o status 404 se a mensagem não for encontrada
+ */
 $messages->delete('/{id}', function ($id) use ($app) {
     $message = $app['orm.em']
         ->getRepository('MicroMessage\Entities\Message')
@@ -65,6 +118,21 @@ $messages->delete('/{id}', function ($id) use ($app) {
     return new Response('', 204);
 });
 
+/**
+ * PUT /{id}
+ * Atualiza uma mensagem existente (não permite criação de novas mensagens,
+ * nesse WebService os ids devem ser gerados automaticamente)
+ *
+ * PARÂMETROS:
+ *  - id int obrigatório
+ *
+ * PARÂMETROS POST:
+ *  - author string(32) obrigatório
+ *  - message string(140) obrigatório
+ *
+ * Retorna o status 204 se a mensagem foi atualizada com sucesso
+ * Retorna o status 404 se a mensagem não for encontrada
+ */
 $messages->put('/{id}', function (Request $request, $id) use ($app) {
     $message = $app['orm.em']
         ->getRepository('MicroMessage\Entities\Message')
